7aa30821e05cf6165105a92290f301e864189a99
pikpik-carrot <100093626+pikpik-carrot@users.noreply.github.com>
2025-05-30 18:51:43 -0500

    Initial commit

---
+++ EotUFly/EotUFly.cs
@@ -0,0 +1,111 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Events;
+using HarmonyLib;
+public interface INewHorizons;
+
+namespace EotUFly
+{
+    public static class Extensions
+    {
+        public static void print<T>(this T t)
+        {
+            EotUFly.Instance.ModHelper.Console.WriteLine($"{t}");
+        }
+    }
+
+    [HarmonyPatch]
+    public class EotUFly : ModBehaviour
+    {
+        bool HasDone = false;
+
+        public static EotUFly Instance;
+        private void Awake()
+        {
+            Instance = this;
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        
+        private void EotUSettings(string s)
+        {
+            if (s != "Eye of the Universe")
+            {
+                "Name is not eye".print();
+                return;
+            }
+
+            if (HasDone)
+            {
+                return;
+            }
+
+            HasDone = true;
+
+
+            s.print();
+
+
+            var root = GameObject.Find("SolarSystemRoot");
+
+            var ESAC1 = root.AddComponent<EyeStateActivationController>();
+            ESAC1._object = GameObject.Find("SixthPlanet_Root");
+            ESAC1._activeStates = [EyeState.WarpedToSurface, EyeState.IntoTheVortex];
+
+            var ESAC2 = root.AddComponent<EyeStateActivationController>();
+            ESAC2._object = GameObject.Find("SignalJammer_Pivot");
+            ESAC2._activeStates = [EyeState.WarpedToSurface];
+
+            var ESAC3 = root.AddComponent<EyeStateActivationController>();
+            ESAC3._object = GameObject.Find("Sector_Campfire");
+            ESAC3._activeStates = [EyeState.InstrumentHunt, EyeState.JamSession, EyeState.BigBang];
+
+            var Eye = GameObject.Find("EyeoftheUniverse_Body").GetComponent<OWRigidbody>();
+
+            var EyeRB = Eye.GetComponent<OWRigidbody>();
+            var SectorSetter = GameObject.Find("Sector_EyeOfTheUniverse");
+            SectorSetter.GetComponent<Sector>()._attachedOWRigidbody = EyeRB;
+
+            var EyeQM = GameObject.Find("EyeoftheUniverse_Body");
+
+            EyeQM.AddComponent<QuantumOrbit>();
+
+            EyeQM.GetComponent<QuantumOrbit>()._orbitRadius = 6000;
+            EyeQM.GetComponent<QuantumOrbit>()._stateIndex = 5;
+
+            var locationsix = GameObject.Find("Sun_Body").GetComponent<QuantumOrbit>();
+            Destroy(locationsix);
+
+        }
+
+        private void Start()
+        {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(EotUFly)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            newHorizons.LoadConfigs(this);
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+
+                if (loadScene == OWScene.SolarSystem)
+                {
+                    var root = GameObject.Find("SolarSystemRoot");
+                    var ESM = root.AddComponent<EyeStateManager>();
+                    ESM._initialState = EyeState.WarpedToSurface;
+                }
+            };
+
+            newHorizons.GetBodyLoadedEvent().AddListener(EotUSettings);
+
+        }
+    }
+}
---
+++ EotUFly/MyPatchClass.cs
@@ -0,0 +1,134 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace EotUFly
+{
+    [Harmony]
+    internal class MyPatchClass
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(QuantumMoon), nameof(QuantumMoon.ChangeQuantumState))]
+        public static bool ChangeQuantumState(QuantumMoon __instance, bool skipInstantVisibilityCheck, ref bool __result)
+        {
+            bool flag = false;
+            if (__instance._isPlayerInside && __instance._hasSunCollapsed)
+            {
+                __result = false;
+                return false;
+            }
+            if (Time.time - __instance._playerWarpTime < 1f)
+            {
+                __result = false;
+                return false;
+            }
+            if (__instance._stateIndex == 5 && __instance._isPlayerInside && !__instance.IsPlayerEntangled())
+            {
+                __result = false;
+                return false;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                int num = ((__instance._collapseToIndex != -1) ? __instance._collapseToIndex : __instance.GetRandomStateIndex());
+                int num2 = -1;
+                for (int j = 0; j < __instance._orbits.Length; j++)
+                {
+                    if (__instance._orbits[j].GetStateIndex() == num)
+                    {
+                        num2 = j;
+                        break;
+                    }
+                }
+                if (num2 == -1)
+                {
+                    Debug.LogError("QUANTUM MOON FAILED TO FIND ORBIT FOR STATE " + num);
+                }
+                float num3 = ((num2 != -1) ? __instance._orbits[num2].GetOrbitRadius() : 10000f);
+                OWRigidbody oWRigidbody = ((num2 != -1) ? __instance._orbits[num2].GetAttachedOWRigidbody() : Locator.GetAstroObject(AstroObject.Name.Sun).GetOWRigidbody());
+                Vector3 onUnitSphere = Random.onUnitSphere;
+                if (num == 6)
+                {
+                    onUnitSphere.y = 0f;
+                    onUnitSphere.Normalize();
+                }
+                Vector3 position = onUnitSphere * num3 + oWRigidbody.GetWorldCenterOfMass();
+                if (!Physics.CheckSphere(position, __instance._sphereCheckRadius, OWLayerMask.physicalMask) || __instance._collapseToIndex != -1)
+                {
+                    __instance._visibilityTracker.transform.position = position;
+                    if (!Physics.autoSyncTransforms)
+                    {
+                        Physics.SyncTransforms();
+                    }
+                    if (skipInstantVisibilityCheck || __instance.IsPlayerEntangled() || !__instance.CheckVisibilityInstantly())
+                    {
+                        __instance._moonBody.transform.position = position;
+                        if (!Physics.autoSyncTransforms)
+                        {
+                            Physics.SyncTransforms();
+                        }
+                        __instance._visibilityTracker.transform.localPosition = Vector3.zero;
+                        __instance._constantForceDetector.AddConstantVolume(oWRigidbody.GetAttachedGravityVolume(), inheritForceAcceleration: true, clearOtherFields: true);
+                        Vector3 vector = oWRigidbody.GetVelocity();
+                        if (__instance._useInitialMotion)
+                        {
+                            InitialMotion component = oWRigidbody.GetComponent<InitialMotion>();
+                            vector = ((component != null) ? component.GetInitVelocity() : Vector3.zero);
+                            __instance._useInitialMotion = false;
+                        }
+                        __instance._moonBody.SetVelocity(OWPhysics.CalculateOrbitVelocity(oWRigidbody, __instance._moonBody, Random.Range(0, 360)) + vector);
+                        __instance._useInitialMotion = false;
+                        __instance._lastStateIndex = __instance._stateIndex;
+                        __instance._stateIndex = num;
+                        __instance._collapseToIndex = -1;
+                        flag = true;
+                        for (int k = 0; k < __instance._stateSkipCounts.Length; k++)
+                        {
+                            __instance._stateSkipCounts[k] = ((k != __instance._stateIndex) ? (__instance._stateSkipCounts[k] + 1) : 0);
+                        }
+                        break;
+                    }
+                    __instance._visibilityTracker.transform.localPosition = Vector3.zero;
+                }
+                else
+                {
+                    Debug.LogError("Quantum moon orbit position occupied! Aborting collapse.");
+                }
+            }
+            if (flag)
+            {
+                if (__instance._isPlayerInside)
+                {
+                    __instance.SetSurfaceState(__instance._stateIndex);
+                }
+                else
+                {
+                    __instance.SetSurfaceState(-1);
+                }
+                GlobalMessenger<OWRigidbody>.FireEvent("QuantumMoonChangeState", __instance._moonBody);
+                __result = true;
+                return false;
+            }
+            __result = false;
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(EyeStateManager), nameof(EyeStateManager.Awake))]
+        public static bool Awake(EyeStateManager __instance)
+        {
+            if (PlayerData.GetWarpedToTheEye())
+            {
+                __instance._initialState = EyeState.AboardVessel;
+                return false;
+            }
+            return false;
+        }
+
+
+    }
+}
